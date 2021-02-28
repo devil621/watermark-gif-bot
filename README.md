01376ab16f328755ac565c1762fd7fc21f15d647
aahnik <66209958+aahnik@users.noreply.github.com>
2021-02-28 23:30:24 +0530

    add progess

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 A telegram bot that helps you apply watermark an image onto GIFs.
 
+# Not yet ready to use
+
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/aahniks/watermark-gif-bot)
 
--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
+import logging
 from settings import API_ID,API_HASH,BOT_TOKEN,SESSION_STRING
+
+
+logging.basicConfig(level=logging.INFO)
 
 if BOT_TOKEN:
     client = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
@@ -9,9 +13,16 @@ elif SESSION_STRING:
 
 
 @client.on(events.NewMessage())
-async def echo(event):
+async def echo(event:events.NewMessage):
+    if event.gif:
+        await event.respond('Got a gif')
+        gif_file = await event.download_media()
+        print(gif_file)
 
-    await event.respond(event.text)
+    else:
+        await event.respond('Not a gif')
+
+
 
 
 def main():
