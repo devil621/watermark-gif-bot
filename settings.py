7bccad5f848d1a4f46194b19efeac3b6b3f28b4d
aahnik <daw@aahnik.dev>
2021-03-01 20:51:42 +0530

    fetch more environment variables
    watermark, xoff, yoff

--- settings.py
+++ settings.py
@@ -7,5 +7,8 @@ API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 SESSION_STRING = os.getenv('SESSION_STRING')
-SECRET_KEY = os.getenv('SECRET_KEY')
+WATERMARK = os.getenv(
+    'WATERMARK', 'https://user-images.githubusercontent.com/66209958/109513526-35883200-7acb-11eb-97ed-c0b2ca72119a.png')
 
+X_OFF = int(os.getenv('X_OFF', '10'))
+Y_OFF = int(os.getenv('Y_OFF', '10'))
