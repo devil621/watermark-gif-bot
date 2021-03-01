4ad5bc558f9cf8dfd7b9272b3c82cf5c2e267ddd
aahnik <daw@aahnik.dev>
2021-03-01 23:49:50 +0530

    fix  Invalid URL '': error
     Invalid URL '': No schema supplied. Perhaps you meant http://?

--- settings.py
+++ settings.py
@@ -8,7 +8,10 @@ API_HASH = os.getenv('API_HASH')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 SESSION_STRING = os.getenv('SESSION_STRING')
 WATERMARK = os.getenv(
-    'WATERMARK', 'https://user-images.githubusercontent.com/66209958/109513526-35883200-7acb-11eb-97ed-c0b2ca72119a.png')
+    'WATERMARK')
+
+if not WATERMARK:
+    WATERMARK = 'https://user-images.githubusercontent.com/66209958/109513526-35883200-7acb-11eb-97ed-c0b2ca72119a.png'
 
 X_OFF = int(os.getenv('X_OFF', '10'))
 Y_OFF = int(os.getenv('Y_OFF', '10'))
