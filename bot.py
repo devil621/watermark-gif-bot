a34c129fb32e2b380c1c4acb26aff516440e5374
aahnik <daw@aahnik.dev>
2021-03-01 19:10:38 +0530

    fix file names, and add start command

--- bot.py
+++ bot.py
@@ -1,35 +1,39 @@
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 import logging
-from settings import API_ID,API_HASH,BOT_TOKEN,SESSION_STRING
-from watermark import watermark_mp4,watermark_gif,mp4_to_gif
+from settings import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
+from watermark import watermark_gif, mp4_to_gif
 
 logging.basicConfig(level=logging.INFO)
 
 if BOT_TOKEN:
     client = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 elif SESSION_STRING:
-    client = TelegramClient(StringSession(SESSION_STRING),API_ID,API_HASH)
+    client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
+
+
+@client.on(events.NewMessage(pattern='/start'))
+async def start(event):
+    await event.respond('Hi! I am alive.')
+    raise events.StopPropagation
 
 
 @client.on(events.NewMessage())
-async def echo(event:events.NewMessage):
+async def echo(event):
     if event.gif:
         await event.respond('Got a gif')
-        mp4_file = await event.download_media()
+        mp4_file = await event.download_media('files/')
         gif_file = mp4_to_gif(mp4_file)
-        outf = watermark_gif(gif_file,'image.png',10,10)
-        await client.send_file(event.sender,'out.gif')
-
-
+        outf = watermark_gif(gif_file, 'files/image.png', 10, 10)
+        print(outf)
+        await client.send_file(event.sender_id, outf)
     else:
         await event.respond('Not a gif')
 
 
-
-
 def main():
     client.run_until_disconnected()
+
 
 if __name__ == '__main__':
     main()
