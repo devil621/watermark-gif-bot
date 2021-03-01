44d35de956012debf8edf8c5c08956f9bd5af61c
aahnik <daw@aahnik.dev>
2021-03-01 20:50:35 +0530

    give final touches

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 import logging
-from settings import API_ID, API_HASH, BOT_TOKEN, SESSION_STRING
+from settings import (API_ID, API_HASH,
+                      BOT_TOKEN, SESSION_STRING,
+                      WATERMARK,
+                      X_OFF, Y_OFF)
 from watermark import watermark_gif, mp4_to_gif
+from utils import download_image, files
+
 
 logging.basicConfig(level=logging.INFO)
 
@@ -24,7 +29,7 @@ async def echo(event):
         await event.respond('Got a gif')
         mp4_file = await event.download_media('files/')
         gif_file = mp4_to_gif(mp4_file)
-        outf = watermark_gif(gif_file, 'files/image.png', 10, 10)
+        outf = watermark_gif(gif_file, files('image.png'), X_OFF, Y_OFF)
         print(outf)
         await client.send_file(event.sender_id, outf)
     else:
@@ -32,6 +37,7 @@ async def echo(event):
 
 
 def main():
+    download_image(url=WATERMARK, filename=files('image.png'))
     client.run_until_disconnected()
 
 
