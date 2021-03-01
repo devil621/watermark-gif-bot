7960a5e7f57ccb0115b2dd1f942ead65ebe90083
aahnik <daw@aahnik.dev>
2021-03-01 20:52:00 +0530

    add a function for downloading image

--- utils.py
+++ utils.py
@@ -1,4 +1,20 @@
 import os
+import requests
+import shutil
+
+
+def download_image(url: str, filename: str) -> bool:
+    try:
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            with open(filename, 'wb') as file:
+                response.raw.decode_content = True
+                shutil.copyfileobj(response.raw, file)
+    except Exception as err:
+        print(err)
+        return False
+    else:
+        return True
 
 
 def files(path: str) -> str:
