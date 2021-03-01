a42c642e91c4126c6a7ad0fc44b84c1d4a042a55
aahnik <daw@aahnik.dev>
2021-03-01 18:48:01 +0530

    clean watermark.py

--- watermark.py
+++ watermark.py
@@ -1,59 +1,49 @@
 from PIL import Image, ImageSequence
 from moviepy.editor import *
+from utils import files
 
-def watermark_gif(background: str, foreground: str, xoff: int = 0, yoff: int = 0):
+
+def mp4_to_gif(mp4_file: str) -> str:
+    ''' Convert an mp4 to gif
+
+    Args:
+        mp4_file (str): path of mp4 file
+
+    Returns:
+        str: path of gif file
+    '''
+
+    clip = VideoFileClip(mp4_file)
+    outfile = files(f'{mp4_file}.gif')
+    clip.write_gif(outfile)
+    return outfile
+
+
+def watermark_gif(bg: str, fg: str, xoff: int = 0, yoff: int = 0) -> str:
     ''' Apply watermark on an GIF
 
     Args:
-        background (str): the GIF image on which the watermark will be applied
-        foreground (str): the watermark
-        xoff (int, optional): x-offset Defaults to 0
-        yoff (int, optional): y-offset Defaults to 0
+        bg (str): background gif
+        fg (str): the watermark
+        xoff (int, optional): x-offset (Defaults to 0)
+        yoff (int, optional): y-offset (Defaults to 0)
+
+    Returns:
+        str: the name of generated file
     '''
 
-    def addOverlay(frame: Image):
-        watermark = Image.open(foreground).convert('RGBA')
+    def addOverlay(frame):
+        watermark = Image.open(fg).convert('RGBA')
         frame.convert('RGBA')
         frame.paste(watermark, (xoff, yoff), watermark)
         return frame
 
-    gif = Image.open(background)
+    gif = Image.open(bg)
     frames = ImageSequence.all_frames(gif, addOverlay)
-    frames[0].save('out.gif', save_all=True,
+
+    outfile = files(f'{bg}_watermarked.gif')
+
+    frames[0].save(outfile, save_all=True,
                    append_images=frames[1:], quality=85)
 
-
-def watermark_mp4(video_file:str,watermark_text:str):
-
-
-    # The video file with audio enabled
-    my_clip = VideoFileClip(video_file, audio=True)
-
-    w, h = my_clip.size  # size of the clip
-
-    # A CLIP WITH A TEXT AND A BLACK SEMI-OPAQUE BACKGROUND
-
-    txt = TextClip(watermark_text, font='Amiri-regular',
-                   color='red', fontsize=24)
-
-    txt_col = txt.on_color(size=(my_clip.w + txt.w, txt.h-10),
-                           color=(0, 0, 0), pos=(6, 'center'), col_opacity=0.6)
-
-    # This example demonstrates a moving text effect where the position is a function of time(t, in seconds).
-    # You can fix the position of the text manually, of course. Remember, you can use strings,
-    # like 'top', 'left' to specify the position
-    txt_mov = txt_col.set_pos(lambda t: (max(w/30, int(w-0.5*w*t)),
-                                         max(5*h/6, int(100*t))))
-
-    # Write the file to disk
-    final = CompositeVideoClip([my_clip, txt_mov])
-    final.duration = my_clip.duration
-    out_file = f'OUT_{video_file}'
-    final.write_videofile(out_file, fps=24, codec='libx264')
-    return out_file
-
-def mp4_to_gif(mp4_file):
-    clip = VideoFileClip(mp4_file)
-    output=f'{mp4_file}.gif'
-    clip.write_gif(output)
-    return output
+    return outfile
