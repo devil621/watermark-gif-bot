6c7f5d66d90fb302d6513304402c06c011639b9e
aahnik <daw@aahnik.dev>
2021-03-02 10:38:53 +0530

    fix -tune error

--- watermark.py
+++ watermark.py
@@ -2,9 +2,10 @@ from settings import X_OFF, Y_OFF
 import os
 import ffmpeg
 
+
 def watermark(video_file: str):
     outf = f'watered_{video_file}'
-    command = f'ffmpeg -i {video_file} -i image.png -preset ultrafast -tune "fastdecode zerolatency" -filter_complex "overlay={X_OFF}:{Y_OFF}" {outf}'
+    command = f'ffmpeg -i {video_file} -i image.png -preset ultrafast -tune fastdecode  -filter_complex "overlay={X_OFF}:{Y_OFF}" {outf}'
     print(command)
     os.system(command)
     return outf
