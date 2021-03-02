d6fee71d41c456503e841d98af87cb8b8a8a6ea4
aahnik <daw@aahnik.dev>
2021-03-02 10:23:58 +0530

    trying to increase speed

--- watermark.py
+++ watermark.py
@@ -1,10 +1,14 @@
 from settings import X_OFF, Y_OFF
 import os
-
+import ffmpeg
 
 def watermark(video_file: str):
     outf = f'watered_{video_file}'
-    command = f'ffmpeg -i {video_file} -i image.png -preset ultrafast -filter_complex "overlay={X_OFF}:{Y_OFF}" {outf}'
+    command = f'ffmpeg -i {video_file} -i image.png -preset ultrafast -tune "fastdecode zerolatency" -filter_complex "overlay={X_OFF}:{Y_OFF}" {outf}'
     print(command)
     os.system(command)
     return outf
+
+# def watermark(video_file:str):
+#     stream = ffmpeg.input(video_file)
+#     ffmpeg.overlay()
