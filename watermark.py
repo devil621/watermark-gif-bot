8cfdcd1b2cbf1cbd32dd06f789dc12c806d7bc99
aahnik <daw@aahnik.dev>
2021-03-02 17:54:09 +0530

    use zero latency

--- watermark.py
+++ watermark.py
@@ -1,11 +1,11 @@
 from settings import X_OFF, Y_OFF
 import os
-import ffmpeg
+# import ffmpeg
 
 
 def watermark(video_file: str):
     outf = f'watered_{video_file}'
-    command = f'ffmpeg -i {video_file} -i image.png -preset ultrafast -tune fastdecode  -filter_complex "overlay={X_OFF}:{Y_OFF}" {outf}'
+    command = f'ffmpeg -i {video_file} -i image.png -preset ultrafast -tune zerolatency  -filter_complex "overlay={X_OFF}:{Y_OFF}" {outf}'
     print(command)
     os.system(command)
     return outf
